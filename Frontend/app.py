cf6b10df909ffca9bbd06660c1b089a0bae6943b
SvanikM <svanikmareddy16@gmail.com>
2026-07-25 12:47:18 -0400

    Added the basic structure for the login page, needs to be connected to backend for functionality

--- Frontend/app.py
+++ Frontend/app.py
@@ -1,4 +1,17 @@
 import streamlit as st
+import requests
+
+FastAPI_URL = "http://localhost:8000"
 
 st.title("Pantry App")
 st.write("Welcome to the Pantry App!")
+
+UserNameInput = st.text_input("Enter your username:")
+EmailInput = st.text_input("Enter your email:")
+PasswordInput = st.text_input("Enter your password:", type="password")
+
+#Uses the get endpoint NEEDS TO BE CONNECTED TO THE BACKEND
+LoginButton = st.button("Login")
+
+#Uses the post endpoint NEEDS TO BE CONNECTED TO THE BACKEND
+SignupButton = st.button("Sign Up")
