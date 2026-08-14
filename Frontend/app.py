129c5412198697e730e80656d1dfe3438c4bbb7e
SvanikM <svanikmareddy16@gmail.com>
2026-08-14 17:25:58 -0400

    Moved the login logic inside of a conditional so it only displays login screen when user data is not stored in session state

--- Frontend/app.py
+++ Frontend/app.py
@@ -7,34 +7,57 @@ FastAPI_URL = "http://localhost:8000"
 st.title("Pantry App")
 st.write("Welcome to the Pantry App!")
 
-#input boxes
-EmailInput = st.text_input("Enter your email:")
-UserNameInput = st.text_input("Enter your username:")
-PasswordInput = st.text_input("Enter your password:", type="password")
+# User info not in session state -> open login page
+if ("username" not in st.session_state) or ("email" not in st.session_state):
+    # Input boxes
+    email_input = st.text_input("Enter your email:")
+    username_input = st.text_input("Enter your username:")
+    password_input = st.text_input("Enter your password:", type="password")
 
-# Uses the post endpoint 
-SignupButton = st.button("Sign Up")
-if SignupButton:
-    response = requests.post(f"{FastAPI_URL}/users", json={"username": UserNameInput, "email": EmailInput, "password": PasswordInput})
-    if response.status_code == 200:
-        st.success("User created successfully!")
-    else:
-        st.error("Error creating user")
+    # Uses the post endpoint
+    signup_button = st.button("Sign Up")
+    if signup_button:
+        response = requests.post(
+            f"{FastAPI_URL}/users",
+            json={
+                "username": username_input,
+                "email": email_input,
+                "password": password_input,
+            },
+        )
+        if response.status_code == 200:
+            st.success("User created successfully!")
+        else:
+            st.error("Error creating user")
 
-# Uses the get endpoint
-# A 200 error code only means the sql query went through, not that a user was found, so the code checks the response to see if the user was found or not
-LoginButton = st.button("Login")
-if LoginButton:
-    response = requests.get(f"{FastAPI_URL}/users", params={"username": UserNameInput, "email": EmailInput})
-    if response.status_code == 200 and response.json().get("message") != "User not found":
-        user_data = response.json()
-        passcheck = requests.get(f"{FastAPI_URL}/users", params={"username": UserNameInput, "email": EmailInput, "password": PasswordInput})
-        user_pass_data = passcheck.json()
-        if user_pass_data.get("message") == "User not found":
-           st.error("Incorrect Credentials")
-        elif user_data.get("users"):
-            st.success("Login successful!")
-    else:
-        st.error("Error logging in")
-
-      
+    # Uses the get endpoint
+    # A 200 error code only means the SQL query went through, not that a user was found, so the code checks the response to see if the user was found or not
+    login_button = st.button("Login")
+    if login_button:
+        response = requests.get(
+            f"{FastAPI_URL}/users",
+            params={"username": username_input, "email": email_input},
+        )
+        if response.status_code == 200 and response.json().get("message") != "User not found":
+            user_data = response.json()
+            passcheck = requests.get(
+                f"{FastAPI_URL}/users",
+                params={
+                    "username": username_input,
+                    "email": email_input,
+                    "password": password_input,
+                },
+            )
+            user_pass_data = passcheck.json()
+            if user_pass_data.get("message") == "User not found":
+                st.error("Incorrect Credentials")
+            elif user_data.get("users"):
+                st.success("Login successful!")
+                st.session_state["email"] = email_input
+                st.session_state["username"] = username_input
+                #rerun causes the script to run again with the session_state variables set, so it leaves the login page after pressing login with the right creds
+                st.rerun()
+        else:
+            st.error("Error logging in")
+elif("email" in st.session_state or "username" in st.session_state):
+    st.write("ur in")
